f3e06fb400455daa99548041d85c3497bc65ba63
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2024-06-16 23:12:22 -0300

    Inicialização do projeto ScreenMathc, inicialização das primeiras variáveis, para sintese do incício do curso

--- Java/PrimeireTentaiva/src/Main.java
+++ Java/PrimeireTentaiva/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("Bem - Vindo ao Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento  = 2022;
+
+        System.out.println("Data de lançamento: "+anoDeLancamento);
+        boolean inclusoNoPlano = false;
+        int notaDoFilme = 10;
+
     }
 }
